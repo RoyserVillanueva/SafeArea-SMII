0c013fa5e95062eeca582b0a08e927bc84f35b5a
JesusEscalante <48699374+JesusEscalante@users.noreply.github.com>
2026-05-12 15:34:45 -0500

    13.5. Sincronizar al reconectar

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,5 +1,6 @@
 // 11.4. Cargar historial de mensajes: Función que realiza el streaming de datos o la consulta de mensajes previos en la conversación.
 // 12.6. Mostrar mensajes en burbujas
+// 13.5. Sincronizar al reconectar
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
